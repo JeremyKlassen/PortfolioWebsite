00e094fc25b44b952d57eb642e93048dba6244cc
jeremy klassen <jeremyklassen@jeremys-MacBook-Air.local>
2023-07-31 20:17:46 -0500

    Added form content for the Contact Me page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import ContactForm from "./ContactForm";
+import Submitted from "./Submitted";
+
 const Contact = () => {
-  return <h1>Contact Me</h1>;
+  const [contactFormState, setContactFormState] = useState(false);
+
+  useEffect(() => {
+    setContactFormState(false);
+  }, []);
+
+  const onSubmit = (newContactState: boolean) => {
+    setContactFormState(newContactState);
+  };
+
+  return (
+    <>
+      <h1>
+        If you would like to get ahold of me, please leave a message and I will
+        get back to you.
+      </h1>
+      {contactFormState ? (
+        <Submitted />
+      ) : (
+        <ContactForm onSubmit={onSubmit} changeContact={contactFormState} />
+      )}
+    </>
+  );
 };
 
 export default Contact;
---
+++ src/components/Contact/Submitted.tsx
@@ -0,0 +1,5 @@
+const Submitted = () => {
+  return <h1>Thank you for your message. I will get back to you soon.</h1>;
+};
+
+export default Submitted;
