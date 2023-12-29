9ea233fb175a3776b8628a0c51829eb0ae891c5c
Jeremy Klassen <jeremy.klassen@gmail.com>
2023-12-29 02:06:45 -0600

    Changed menu to a simpler scheme.

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,104 +1,3 @@
-// import styles from "./Header.module.css";
-// import { useEffect, useState } from "react";
-// import Logo from "./Logo";
-// import NavLinks from "./NavLinks";
-// import Banner from "./Banner";
-// import { GiHamburgerMenu } from "react-icons/gi";
-// import { Link } from "react-router-dom";
-// import { BiSolidRightArrow } from "react-icons/bi";
-
-// const Header = () => {
-//   // components to be used in ternary operators
-//   const burger = () => {
-//     return (
-//       <Link className={styles.hamburgerDiv} onClick={handleClick} to={""}>
-//         <GiHamburgerMenu color="#023047" size={50} />
-//       </Link>
-//     );
-//   };
-
-//   const arrow = () => {
-//     return (
-//       <Link className={styles.arrowDiv} onClick={handleClick} to={""}>
-//         <BiSolidRightArrow color="#023047" size={40} />
-//       </Link>
-//     );
-//   };
-
-//   const logo = () => {
-//     return (
-//       <div className={styles.logo}>
-//         <Logo isMobile={!isDesktop} />
-//       </div>
-//     );
-//   };
-
-//   //state
-//   const [menuShown, setMenuShown] = useState(window.innerWidth > 768);
-//   const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
-
-//   const handleClick = () => {
-//     setMenuShown(!menuShown);
-//   };
-
-//   //changes the banner depending on screenwidth
-//   useEffect(() => {
-//     const handleResize = () => {
-//       const newIsDesktop = window.innerWidth > 768;
-//       setIsDesktop(newIsDesktop);
-//     };
-
-//     const initialSize = () => {
-//       const newIsDesktop = window.innerWidth > 768;
-//       setIsDesktop(newIsDesktop);
-//     };
-
-//     initialSize();
-
-//     window.addEventListener("resize", handleResize);
-
-//     return () => {
-//       window.removeEventListener("resize", handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <section
-//         className={`${styles.headContainer}  ${
-//           menuShown ? styles.headContainerExp : styles.headContainerMin
-//         }`}
-//       >
-//         {menuShown ? (
-//           <>
-//             {logo()}
-//             <NavLinks setMenuShown={setMenuShown} />
-//             {isDesktop ? <></> : arrow()}
-//           </>
-//         ) : (
-//           <>
-//             {logo()}
-//             {burger()}
-//           </>
-//         )}
-//       </section>
-//       {isDesktop && <Banner />}
-//       {!isDesktop ? (
-//         <section className={styles.nameSection}>
-//           <div className={styles.nameContainer}>
-//             <h1 className={styles.mobileH1}>Jeremy Klassen</h1>
-//             <h2 className={styles.mobileH2}>
-//               Web Development & Information Systems
-//             </h2>
-//           </div>
-//         </section>
-//       ) : (
-//         <></>
-//       )}
-//     </>
-//   );
-// };
-
 // export default Header;
 import styles from "./Header.module.css";
 import { useEffect, useState } from "react";
@@ -114,7 +13,7 @@ const Header = () => {
   const burger = () => {
     return (
       <Link className={styles.hamburgerDiv} onClick={handleClick} to={""}>
-        <GiHamburgerMenu color="#023047" size={50} />
+        <GiHamburgerMenu color="#ffffff" size={50} />
       </Link>
     );
   };
@@ -122,7 +21,7 @@ const Header = () => {
   const arrow = () => {
     return (
       <Link className={styles.arrowDiv} onClick={handleClick} to={""}>
-        <BiSolidRightArrow color="#023047" size={40} />
+        <BiSolidRightArrow color="#ffffff" size={40} />
       </Link>
     );
   };
@@ -179,8 +78,8 @@ const Header = () => {
         {isMenuOpen || isDesktop ? (
           <>
             {logo()}
-            <NavLinks setMenuShown={setIsMenuOpen} />
             {isDesktop ? <></> : arrow()}
+            <NavLinks setMenuShown={setIsMenuOpen} />
           </>
         ) : (
           <>
